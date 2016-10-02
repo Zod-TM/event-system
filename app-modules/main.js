a20747b6364f17fb3907e6fa2bb3c862251806ca
george-georgiev <george.georgiev@mail.bg>
2016-10-02 18:10:24 +0300

    Add isLoggednIn check in main.js

--- app-modules/main.js
+++ app-modules/main.js
@@ -1,11 +1,17 @@
 import { register, login, logout, contactUs } from 'usersController';
 import { all as homeAll} from 'homeController';
+import { isLoggedIn } from 'backendServices';
 
 let router = new Navigo(null, false);
 router.on('#register', () => register())
     .on('#login', () => login())
     .on('#', () => homeAll())
+    .on('', () => homeAll())
     .on('#contact', () => contactUs())
     .resolve();
 
 $('#logout').on('click', logout);
+
+if (isLoggedIn()) {
+    $('#menu-items').addClass('logged-in');
+}
